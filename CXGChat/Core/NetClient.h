f95b0a4c22146972ac753c5f4dba989aa809aa94
lixinhang <lixinhang@icloud.com>
2017-03-27 10:47:09 +0800

    修复底层tcp粘包问题
    修复底层tcp粘包问题

--- CXGChat/Core/NetClient.h
+++ CXGChat/Core/NetClient.h
@@ -4,12 +4,7 @@
 #include "network.h"
 #include "Packet.h"
 
-#define RECV_BUF_SIZE 8192
-#define SEND_BUF_SIZE 2048
-#define	HEAD_SIZE	4
-
 using namespace NetWork;
-
 
 enum ERRNO {
     CON_NETNOTREACH = 100001,
@@ -39,7 +34,7 @@ public:
 	virtual void __stdcall GetConfig(PEER_CONFIG& config);
 	virtual void __stdcall UpdateConfig(const PEER_CONFIG& config);
 	virtual bool __stdcall SendData(const char* pData, int nLen);
-	virtual bool __stdcall RecvData();
+	virtual bool __stdcall RecvData(char* pData, int nLen);
 
 	virtual bool __stdcall Connect();
 	virtual bool __stdcall Open();
@@ -52,7 +47,7 @@ public:
 	virtual void __attribute__((__stdcall__)) GetConfig(PEER_CONFIG& config);
 	virtual void __attribute__((__stdcall__)) UpdateConfig(const PEER_CONFIG& config);
 	virtual bool __attribute__((__stdcall__)) SendData(const char* pData, int nLen);
-	virtual bool __attribute__((__stdcall__)) RecvData();
+	virtual bool __attribute__((__stdcall__)) RecvData(char* pData, int nLen);
 
 	virtual bool __attribute__((__stdcall__)) Connect();
 	virtual bool __attribute__((__stdcall__)) Open();
@@ -64,31 +59,30 @@ private:
 	static void* ConnectBengin(void *arg);
 	void ConnectEnd();
 
+	int GetPacketHeadLenth(char* pData);
+
 private:
 
-	int		m_nRetCode;
-	int		m_nRecvSize;
-	int		m_nPacketSize;
+	int	m_nRetCode;
+	int	m_nNeedCopySize;
 	
 	bool	m_bRet;
 	bool	m_bThreadRet;
 	bool	m_bSendRet;
-	bool	m_bReading;
 
 	long    m_nRefCount;
-	char	m_cHeadBuf[HEAD_SIZE];
-	char	m_cFullBuf[RECV_BUF_SIZE];
+	char	m_cLastRecv[RECV_BUF_SIZE];
 
-	PEER_CONFIG		m_NetConfig;
+	PEER_CONFIG	m_NetConfig;
 	INetPeerSink*	m_RawLink;
 
-	pthread_t		m_Connect;
+	pthread_t	m_Connect;
 	pthread_mutex_t	m_Mutex;
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
-	SOCKET			m_socket;
+	SOCKET		m_socket;
 #else
-	int				m_socket;
+	int		m_socket;
 #endif
 };
 
